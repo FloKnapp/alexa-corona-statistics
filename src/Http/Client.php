dd8fa29990d6327c531fdfd5fa3f9b8c083c3a53
Flo Knapp <office@florianknapp.de>
2020-04-18 23:31:47 +0200

    Add primitive client

--- src/Http/Client.php
+++ src/Http/Client.php
@@ -7,11 +7,33 @@ namespace Alexa\Http;
 class Client
 {
 
-    public static function get()
+    private static function request(string $method, string $path, array $headers, array $data)
     {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+
+        switch ($method) {
+
+            case 'POST':
+                curl_setopt($ch, CURLOPT_POST, true);
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+                break;
+            case 'PUT':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+                break;
+            case 'DELETE':
+                curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                break;
+            default:
+                break;
+
+        }
+
+
+
 
     }
-
-    private static function send(string $method,
 
 }
