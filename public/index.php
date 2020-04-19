8d513aff0d89c81a8cc13a7ab5ee929e1bd26b5c
Flo Knapp <office@florianknapp.de>
2020-04-19 16:11:02 +0200

    Test

--- public/index.php
+++ public/index.php
@@ -5,8 +5,10 @@ require_once __DIR__ . '/../vendor/autoload.php';
 header ('Content-Type: application/json');
 
 echo json_encode([
+    'version' => '1.0',
     'response' => [
         'outputSpeech' => [
+            'type' => 'PlainText',
             'text' => 'hallo'
         ]
     ]
