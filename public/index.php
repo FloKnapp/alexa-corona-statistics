d5febc38799e7289181bec4b0636d0de687e965e
Flo Knapp <office@florianknapp.de>
2020-04-19 20:12:22 +0200

    Fix entity matching of request

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 use Alexa\Http\Client;
@@ -7,7 +10,10 @@ use Alexa\Entity\AlexaRequest;
 use Alexa\Service\CoronaDataAggregator;
 use Alexa\Controller\IntentController;
 
-$requestBody = file_get_contents('php://stdin');
+header('Accept: application/json');
+
+$requestBody = file_get_contents('php://input');
+
 $request     = new AlexaRequest($requestBody);
 
 $client     = new Client();
